907ce06bf4a54502244fabbaa40538cf3905c98c
Michael Carroll <mjcarroll@ieee.org>
2010-08-29 14:59:10 -0500

    Added MATLAB-generated documentation for functions.
    This covers Exercise 1, Exercise 2-5, and Exercise 7

--- Project1/Exercise1.m
+++ Project1/Exercise1.m
@@ -1,10 +1,16 @@
 function x = Exercise1( L, A, b, M )
-%Exercise1  Generate a periodic, truncated, decaying exponential function
+%% Exercise1  
+% Generate a periodic, truncated, decaying exponential function:
 % L - Total number of samples in the waveform
 % A - Beginning amplitude of the exponential function
 % b - Decay rate of the exponential function
 % M - Number of samples each period will last
-% The function takes the form Aexp(-bn)
+% The function takes the form $A*\exp{-bn}$
+% Test Paramters:
+% L = 80
+% A = 2
+% b = 0.08
+% M = 20
 
 x = A*exp(-b * mod([0:L-1],M));
 
