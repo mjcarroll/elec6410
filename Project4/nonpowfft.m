1b713a3b4b1e2d5778ca185af5c8b27549f91a61
Michael Carroll <mjcarroll@ieee.org>
2010-10-08 01:16:35 -0500

    Non power of 2 fft appears to be working.
    I cleaned up my variable names to reflect the ones from the bluestein algorithm on Wikipedia.

--- Project4/nonpowfft.m
+++ Project4/nonpowfft.m
@@ -1,31 +1,40 @@
-x = [1 1 1 1 1];
+x=[1 1 1 1 1];
 
-% Preallocate the array for speed.
-N = length(x);
-X = zeros([1,N]);
+% References:
+% http://www.engineeringproductivitytools.com/stuff/T0001/PT11.HTM#Head617
+% http://www.katjaas.nl/chirpZ/chirpZ.html
+% http://en.wikipedia.org/wiki/Bluestein%27s_FFT_algorithm
+% http://healpix.jpl.nasa.gov/html/libfftpack/bluestein_8c-source.html
+% I'm using the notation for Bluestein's algorithm from wikipedia.
 
+N=length(x);
+% We want the convolution index to be > 2N-1
 Nf = 2^nextpow2(2*N-1);
 
-y = zeros([1,Nf-1]);
-
-for n=1:N
-    y(n) = exp(1i*pi*(n-1)^2/N);
-    y(Nf-(n)) = y(n);
-end
-
-Y = fft(y,Nf);
-
-f = zeros([1,Nf-1]);
+% Coefficient a_{n} from Bluestein's algorithm.
+a = zeros([1,Nf]);
 for n=1:N,
-    f(n) = x(n)*exp(-1i*pi*(n-1)^2/N);
+    a(n)=x(n).*exp(-1i*pi*(n-1)^2/N);
 end
 
-F = fft(f,Nf);
-
-C = ifft(Y.*F);
-
-for n = 1:N,
-    X(n) = exp(-1i*pi*n^2/N)/Nf * C(n);
+% Coefficient b_{n} from Bluestein's algorithm.
+b = zeros([1,Nf]);
+b(1)=1;
+for n=2:N,
+    b(n)=exp(1i*pi*(n-1)^2/N);
+    b(Nf-(n-2))=b(n);
 end
 
-X
+% Convolve a_{n} and b_{n} together.
+% Using Nf as an argument for the fft guarantees a power-of-2 input.
+% We could just as easily zero-pad and convolve.
+A = fft(a,Nf);
+B = fft(b,Nf);
+y = ifft(A.*B,Nf);
+
+% Coefficient b_{k} from Bluestein's algorithm.
+Y = zeros([1,N]);
+for n=1:N
+    Y(n) = y(n).*exp(-1i*pi*(n-1)^2/M);
+end
+
