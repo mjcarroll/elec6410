fcc87db5ef0b8f7d6c44cda46d1039cfb7b47f45
Michael Carroll <mjcarroll@ieee.org>
2010-10-07 20:40:55 -0500

    Added my custom implementation of the DFT sum.

---
+++ Project4/dftmjc.m
@@ -0,0 +1,7 @@
+function [ output_sequence ] = dftmjc( sequence )
+%dftmjc Computes the DFT summation of an input sequence.
+%   Michael's (probably bad) implementation using loops and whatnot.
+
+
+end
+
